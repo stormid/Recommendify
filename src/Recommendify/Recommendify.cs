35ef9d212ecf5c959c024c4e7dc1d4a45e575242
Rory Fitzpatrick <rory@stormid.com>
2012-07-13 16:51:54 +0100

    Remove internal keyword from RedisClient (really needs to be injected)

--- src/Recommendify/Recommendify.cs
+++ src/Recommendify/Recommendify.cs
@@ -7,6 +7,11 @@ namespace Recommendify
         public const int DEFAULT_MAX_NEIGHBOURS = 50;
         public const string DEFAULT_REDIS_PREFIX = "recommendify";
 
-        internal static IRedisClient RedisClient { get; private set; }
+        static Recommendify()
+        {
+            RedisClient = new RedisClient("localhost");
+        }
+
+        public  static IRedisClient RedisClient { get; private set; }
     }
 }
